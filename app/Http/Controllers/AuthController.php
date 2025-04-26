74bf7c2c99096ab3b37ba014d45ba4d6ffa67ddb
mohamed <mohamedboukab202@gmail.com>
2025-04-26 12:54:23 +0100

    Fix: DELIV-8 - auth connexion by google

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,8 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
-use Exception;
-use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
